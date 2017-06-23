6e8a081876edf1a538c49f724f12421f159f165c
eqoh <o.hercht@equitania.com>
2017-06-23 13:54:58 +0200

    [CHG] Update der Versionsnr

--- equitania/__openerp__.py
+++ equitania/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Equitania Erweiterungsmodul',
     'license': 'AGPL-3',
-    'version': '1.1.168',
+    'version': '1.1.169',
     'description': """
         Equitania Software GmbH
     """,
